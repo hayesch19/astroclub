2eafbdc9f86f7c7a2ca06580231c529b72b9bb78
hayesch19 <chrishayespro@gmail.com>
2019-08-18 13:29:42 -0400

    V2 APIs Console Log

--- public/main.js
+++ public/main.js
@@ -1,7 +1,38 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const unicorn = () => {
+  nasaUrl()
+  getMainImage()
+  spaceXUrl()
+  getLaunchCard()
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// Nasa API
+const nasaUrl = nasaSite => {
+  const API_URL = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+  return API_URL
+}
+
+// SpaceX API
+const spaceXUrl = spaceXSite => {
+  const SPACE_API_URL =
+    'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+  return SPACE_API_URL
+}
+
+// Get NASA Image
+const getMainImage = async nasaSite => {
+  // eslint-disable-next-line no-undef
+  const response = await fetch(nasaUrl(nasaSite))
+  const nasaData = await response.json()
+
+  console.log(nasaData)
+}
+
+// SpaceX Launch Info
+const getLaunchCard = async spaceXSite => {
+  // eslint-disable-next-line no-undef
+  const response = await fetch(spaceXUrl(spaceXSite))
+  const spaceXData = await response.json()
+
+  console.log(spaceXData)
+}
+document.addEventListener('DOMContentLoaded', unicorn)
